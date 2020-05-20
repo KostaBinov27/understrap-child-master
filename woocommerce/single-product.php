40eedf60ec1321adc35fcd30d6af9da436056c06
kostabinov <kostabinovps@gmail.com>
2020-05-20 20:24:58 +0200

    Related posts in single post get out of container

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -55,7 +55,19 @@ get_header( 'shop' ); ?>
 		 */
 		do_action( 'woocommerce_sidebar' );
 	?>
+		<?php
+	/**
+	 * Hook: woocommerce_after_single_product_summary.
+	 *
+	 * @hooked woocommerce_output_product_data_tabs - 10
+	 * @hooked woocommerce_upsell_display - 15
+	 * @hooked woocommerce_output_related_products - 20
+	 */
+	do_action( 'woocommerce_after_single_product_summary' );
+	?>
+</div>
 
+<?php do_action( 'woocommerce_after_single_product' ); ?>
 <?php
 get_footer( 'shop' );
 
